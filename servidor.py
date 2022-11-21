f8e5a98c1b47e5d3846d0a6d250130f68324e3f3
Marcio <marciojsilva159@gmail.com>
2022-11-21 02:12:18 -0300

    Implementando código

--- servidor.py
+++ servidor.py
@@ -1,2 +1,24 @@
 #TODO Fazer o servidor multithreads.
+import socket
+from time import sleep
 import threading
+
+HOST = 'localhost'
+PORT = 5000
+
+udp = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+udp.bind((HOST, PORT))
+
+print('Servidor no ar... Faça seu pedido')
+
+def trata_cliente(msg, cliente):
+    print('Recebi de', cliente, 'a mensagem', msg.decode())
+    udp.sendto(('OK-' + msg.decode()).encode(), cliente)
+
+while True:
+    msg, cliente = udp.recvfrom(1024)
+    t = threading.Thread(target=trata_cliente, args=(msg, cliente,))
+    t.start()
+
+udp.close()
