4e68a368abcc425649b806498df3295381100120
AllanSmithll <allanammancio2004@gmail.com>
2022-11-29 22:21:31 -0300

    Atualizaçãozinha

--- imprimir.py
+++ imprimir.py
@@ -5,7 +5,7 @@ mutex = Semaphore(1)
 listaDePoltronasDesocupadas= [0,1,2,3,4,5,6,7,8,9,10]
 if listaDePoltronasDesocupadas != None:
     poltrona= listaDePoltronasDesocupadas[random.randint(0,10)]
-    print(f' ----------Nota fiscal------- \n Emitido em : São Miguel de Taipu \n Agência: 40028922 \n Origem: Local atual \n Destino:João Pessoa-PB \n Data/Hora:{date.today()}  \n Poltrona: {poltrona} ' )
+    print(f' ----------Nota fiscal------- \n Emitido em: São Miguel de Taipu \n Agência: 40028922 \n Origem: Local atual \n Destino:João Pessoa-PB \n Data/Hora:{date.today()}  \n Poltrona: {poltrona} ' )
     listaDePoltronasOcupadas= []
     temp = 0
     listaDePoltronasOcupadas.append(listaDePoltronasDesocupadas.pop(poltrona))
