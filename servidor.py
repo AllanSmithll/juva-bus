83c151cb43d36c2d93024ff35c307e1b10f71012
Michaelleonny <michaelleonny@live.com>
2022-12-07 12:49:45 -0300

    melhorando

--- servidor.py
+++ servidor.py
@@ -2,6 +2,8 @@
 import socket
 from time import sleep
 import threading
+import random
+from datetime import date
 
 HOST = 'localhost'
 PORT = 5000
@@ -14,10 +16,22 @@ print('Servidor no ar... Faça seu pedido')
 
 def trata_cliente(msg, cliente):
     print('Recebi de', cliente, 'a mensagem', msg.decode())
-    udp.sendto(('OK-' + msg.decode()).encode(), cliente)
+    print()
+    info = str(msg.decode())
+    temp = info.split()
+    print(temp[0] + temp[1])
+    print(temp[2] + temp[3])
+    numeroPoltrona = random.randint(1,20) 
+    hashTable = {temp[3]:numeroPoltrona}
+    print(hashTable)
+    print()
+    nota= f" Cliente: {temp[1]} \n Poltrona: {hashTable[temp[3]]} \n Data:{date.today()}"
+    udp.sendto(nota.encode(), cliente)
 
 while True:
     msg, cliente = udp.recvfrom(1024)
+    print(f'Cliente',cliente)
+    print('Mesangem',msg.decode())
     t = threading.Thread(target=trata_cliente, args=(msg, cliente,))
     sleep(2)
     t.start()
