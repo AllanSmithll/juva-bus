9f13a72f7ad4344afd4532f55cb592f72bc6d3f4
Marcio <marciojsilva159@gmail.com>
2022-12-11 14:53:51 -0300

    Implemetando a lista sequencial

--- servidor.py
+++ servidor.py
@@ -4,7 +4,8 @@ from time import sleep
 import threading
 import random
 from datetime import date
-
+from FuncoesDeApoio.listaSequencial import *
+import random
 HOST = 'localhost'
 PORT = 5000
 
@@ -13,6 +14,24 @@ udp = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 udp.bind((HOST, PORT))
 
 print('Servidor no ar... Faça seu pedido')
+
+listaDePoltronasDesocupadas = Lista()
+for i in range(1,20):
+    listaDePoltronasDesocupadas.inserir(i,i+1)
+print(listaDePoltronasDesocupadas)   
+
+poltrona= listaDePoltronasDesocupadas.elemento(random.randint(0,10))
+print(poltrona)
+
+listaDePoltronasOcupadas = Lista()
+print(listaDePoltronasOcupadas)
+
+listaDePoltronasOcupadas.inserir(1,listaDePoltronasDesocupadas.remover(listaDePoltronasDesocupadas.busca(poltrona)))
+print(listaDePoltronasOcupadas)
+print(listaDePoltronasDesocupadas)
+# if listaDePoltronasDesocupadas != None:
+#      poltrona = listaDePoltronasDesocupadas[random.randint(0,10)]
+
 
 def trata_cliente(msg, cliente):
     print('Recebi de', cliente, 'a mensagem', msg.decode())
@@ -33,7 +52,7 @@ def trata_cliente(msg, cliente):
     print(hashTable)
     print('Hash^^')
 
-    nota= f" ========Sua Nota Fiscal======= \n Cliente: {dados[0]} \n Destino:{dados[4]} \n Poltrona: {hashTable[dados[2]]} \n Data: {date.today()} \n Tipo de Pagamento: {dados[6]}"
+    nota= f" ========Sua Nota Fiscal=======  \n Agência: 40028922 \n Cliente: {dados[0]} \n Destino:{dados[4]} \n Poltrona: {hashTable[dados[2]]} \n Data: {date.today()} \n Tipo de Pagamento: {dados[6]}"
     udp.sendto(nota.encode(), cliente)
 
 while True:
