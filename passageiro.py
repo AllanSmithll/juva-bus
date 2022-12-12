a8046cdf85ac417b636b83f1ccfd2d60ff33d85b
AllanSmithll <101681192+AllanSmithll@users.noreply.github.com>
2022-12-12 13:14:40 +0000

    Pequenas alterações

--- passageiro.py
+++ passageiro.py
@@ -13,7 +13,7 @@ marcio.adicionarCpf(cpf)
 marcio.adicionarNome(nome)
 print()
 print()
-opcao = int(input('============= Menu ============= \nPara comprar passagem, digite 1: \nPara mais sabe preço da passagem, digite 2: \nDigite: '))
+opcao = int(input('============= Menu ============= \nPara comprar passagem, digite 1: \nPara mais saber preço da passagem, digite 2: \nDigite: '))
 
 while True:
     if opcao == 1:
@@ -24,7 +24,7 @@ while True:
         print(msg_servidor.decode())
    
     if opcao == 2:
-        print('Passagem está em R$4,60 \n A meia passagem está R$2,30')
+        print('- Passagem está em R$4,60 \n- A meia passagem está R$2,30')
         comprar = input("Quer fazer uma compra (S ou N)? ").upper()
-        
+        opcao = int(input('============= Menu ============= \nPara comprar passagem, digite 1: \nPara mais saber preço da passagem, digite 2: \nDigite: '))
         
